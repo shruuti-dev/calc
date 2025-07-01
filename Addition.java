92bd29b1387e3cfb2c8a74d4fc48ae94dee77df0
AS <shrutisinghpgcdac@gmail.com>
2025-07-01 10:09:37 +0530

    Written java addition code

---
+++ Addition.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class Addition
+{
+public static void main(String[] args) {
+Scanner sc = new Scanner(System.in);
+ System.out.println("Enter num1: ");
+int num1 = sc.nextInt();
+System.out.println("Enter num2: ");
+int num2 = sc.nextInt();
+int sum = num1 + num2;
+System.out.println("the sum is: " + sum);
+
+sc.close();
+}
+}
